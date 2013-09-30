f47270d6f3cd7649bc5c6cd5beb6649fadd44ade
Richard_D_Dolsick <wk0852@Richard-D-Dolsickui-MacBook-Air.local>
2013-09-30 10:03:48 -0700

    complete

--- lib/hw1309/Web.pm
+++ lib/hw1309/Web.pm
@@ -7,6 +7,10 @@ use encoding 'utf8';
 use Kossy;
 use DBIx::Sunny;
 
+use Data::Dumper;
+
+my $tempEditId = -1;
+
 sub dbh {
 	my $self = shift;
 	$self->{_dbh} ||= DBIx::Sunny->connect( "dbi:mysql:test",'root','1234',{
@@ -32,16 +36,29 @@ EOF
 sub addDb {
     my $self = shift;
     my ( $title, $memo, $priority, $deadline  ) = @_;
-#      my $queryStr = q{INSERT INTO content (title, memo, priority, status, deadline) VALUES ( ?, ?, ?, 0, now() )}, $title, $memo, $priority;
-#      $queryStr = Encode::encode("utf-8", $queryStr);
-#      $self->dbh->query( $queryStr );
-       $self->dbh->query( q{INSERT INTO content (title, memo, priority, status, deadline) VALUES ( ?, ?, ?, 0, now() )}, $title, $memo, $priority );
+	$self->dbh->query( q{INSERT INTO content (title, memo, priority, status, deadline) VALUES ( ?, ?, ?, 0, DATE_FORMAT(?,'%Y/%m/%d %H:%i:%s') )}, $title, $memo, $priority, $deadline );
+}
+
+sub updateDb {
+    my $self = shift;
+	my ( $id, $title, $memo, $priority, $status, $deadline ) = @_;
+	$self->dbh->query( "UPDATE content SET title = '".$title."', memo = '".$memo."', priority = ".$priority.", status = ".$status.", deadline = DATE_FORMAT('".$deadline."','%Y/%m/%d %H:%i:%s') WHERE id = '".$id."';" );
 }
 
 sub getDbList {
     my $self = shift;
     my $rows = $self->dbh->select_all(
         "SELECT * FROM content"
+    );
+    return $rows;
+}
+
+sub getDbById {
+    my $self = shift;
+    my ( $id  ) = @_;
+    my $rows = $self->dbh->select_all(
+        q{SELECT * FROM content WHERE id = ?}
+        , $id
     );
     return $rows;
 }
@@ -92,6 +109,12 @@ get '/' => sub {
     });
 };
 
+get '/list' => sub {
+    my ( $self, $c )  = @_;
+    my $db_inputs = $self->getDbList();
+   	$c->render_json({ db_inputs => $db_inputs }); # send reload url
+};
+
 post '/input' => sub {
     my ( $self, $c )  = @_;
     my $result = $c->req->validator([
@@ -121,7 +144,47 @@ post '/input' => sub {
             ],
         }
     ]);
+
 	$self->addDb( $result->valid->get('title'), $result->valid->get('memo'), $result->valid->get('priority'), $result->valid->get('deadline') );
+   	$c->render_json({ location => $c->req->uri_for("/")->as_string }); # send reload url
+};
+
+post '/modify' => sub { # from edit form
+    my ( $self, $c )  = @_;
+    
+    my $result = $c->req->validator([
+        'edit_title' => {
+		    rule => [
+                ['NOT_NULL','empty title'],
+            ],
+        },
+        'edit_memo' => {
+		    rule => [
+                ['NOT_NULL','empty memo'],
+            ],
+        },
+        'edit_priority' => {
+		    rule => [
+                ['NOT_NULL','empty priority'],
+            ],
+        },
+        'edit_status' => {
+		    rule => [
+                ['NOT_NULL','empty status'],
+            ],
+        },
+        'edit_deadline' => {
+		    rule => [
+                ['NOT_NULL','empty deadline'],
+            ],
+        },
+        'edit_id' => {
+		    rule => [
+                ['NOT_NULL','empty id'],
+            ],
+        },
+    ]);
+    $self->updateDb( $result->valid->get('edit_id'), $result->valid->get('edit_title'), $result->valid->get('edit_memo'), $result->valid->get('edit_priority'), $result->valid->get('edit_status'), $result->valid->get('edit_deadline') );
    	$c->render_json({ location => $c->req->uri_for("/")->as_string }); # send reload url
 };
 
@@ -143,6 +206,33 @@ post '/del' => sub {
     $self->clearDbListOne($result->valid->get('id'));
   	$c->render_json({ location => $c->req->uri_for("/")->as_string }); # send reload url
 };
+
+post '/edit' => sub {
+    my ( $self, $c )  = @_;
+    my $result = $c->req->validator([
+        'id' => {
+		    rule => [
+                ['NOT_NULL','empty id'],
+            ]
+        }
+    ]);
+
+	$tempEditId = $result->valid->get('id');
+   	$c->render_json({ location => $c->req->uri_for("/edit")->as_string }); # send reload url
+#    	$c->render_json({ location => $c->req->uri_for("/edit?id=".$result->valid->get('id'))->as_string }); # send reload url
+    
+};
+
+get '/edit' => sub {
+    my ( $self, $c )  = @_;
+    my $db_outputs = $self->getDbById( $tempEditId );
+
+    $c->render('edit.tx', {
+        db_outputs => $db_outputs,
+        edit_id => $tempEditId,
+    });    
+};
+
 
 
 1;
